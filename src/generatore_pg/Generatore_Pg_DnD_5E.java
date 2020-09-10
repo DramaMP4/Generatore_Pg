9a24ea28b1601d73e6b570dac5d0635a0eeb1e86
Gabriele Giuntoli <lelegiunto1@gmail.com>
2020-09-11 00:36:13 +0200

    commit

--- src/generatore_pg/Generatore_Pg_DnD_5E.java
+++ src/generatore_pg/Generatore_Pg_DnD_5E.java
@@ -21,6 +21,7 @@ public class Generatore_Pg_DnD_5E{
         String[] Razze=new String[]{"Elfo","Gnomo","Halfling","Mezzorco","Mezzelfo","Nano","Umano"};
         String[] Allineamenti=new String[]{"Legale Buono","Legale Neutrale","Legale Malvagio","Neutrale Buono","Neutrale","Neutrale Malvagio","Caotico Buono","Caotico Neutrale","Caotico malvagio"};
         String[] Classi=new String[]{"Barbaro","Bardo","Chierico","Druido","Guerriero","Ladro","Mago","Monaco","Paladino","Ranger","Stregone"};
+        String[] Cammini_Barbaro=new String[]{"Cammino del Berserk","Cammino del Combattente Totemico"};
         String[] Archetipi_Ladreschi=new String[]{"Furfante","Assassino","Mistificatore Arcano"};
         String[] Scuole_Magia=new String[]{"Scuola1"};
         String[] Stirpe_Stregone=new String[]{"Stirpe1"};
@@ -30,32 +31,48 @@ public class Generatore_Pg_DnD_5E{
         System.out.println("Classe : "+Classi[Classe]);
         int Allineamento=random.nextInt(Allineamenti.length);
         System.out.println("Allineamento : "+Allineamenti[Allineamento]);
+        // Cammino Barbaro
+        int Cammino=random.nextInt(Cammini_Barbaro.length);
         //Archetipi Ladro
         int archetipo=random.nextInt(Archetipi_Ladreschi.length);
+        //Scuola di Magia
+        int scuola=random.nextInt(Scuole_Magia.length);
         //Stirpe Stregone 
         int stirpe=random.nextInt(Stirpe_Stregone.length);
+        //Random Cammini Barbaro
+        if(Classe==0){
+            System.out.println("Cammino Barbaro : "+Cammini_Barbaro[Cammino]);
+        }
+        //Random Collegi Bardo
+        if(Classe==1){
+            System.out.println("Collegio Bardico : ");
+        }
         //Random Archetipo Ladro
         if(Classe==5){
             System.out.println("Archetipo : "+Archetipi_Ladreschi[archetipo]);
+        }
+        //Random Scuola di Magia 
+        if (Classe==6){
+            System.out.println("Scuola di Magia : "+Scuole_Magia[scuola]);
         }
         //Random Stirpe Stregone
         if(Classe==10){
             System.out.print("Stirpe : "+Stirpe_Stregone[stirpe]);
         }
         
-        /*int minimo=10,massimo=20;
+        int minimo=10,massimo=20;
         int Forza=random.nextInt(massimo-minimo)+minimo;
         int Carisma= random.nextInt(massimo - minimo) + minimo;
         int Destrezza= random.nextInt(massimo - minimo) + minimo;
         int Saggezza= random.nextInt(massimo - minimo) + minimo;
         int Intelligenza= random.nextInt(massimo - minimo) + minimo;
-        int Costituzione= random.nextInt(massimo - minimo) + minimo;*/
-        /*System.out.println("Forza : "+Forza);
+        int Costituzione= random.nextInt(massimo - minimo) + minimo;
+        System.out.println("Forza : "+Forza);
         System.out.println("Destrezza : " + Destrezza);
         System.out.println("Costituzione: " + Costituzione);
         System.out.println("Intelligenza : " + Intelligenza);
         System.out.println("Saggezza : "+Saggezza); 
-        System.out.println("Carisma : "+Carisma); */
+        System.out.println("Carisma : "+Carisma); 
     }
 }
 
