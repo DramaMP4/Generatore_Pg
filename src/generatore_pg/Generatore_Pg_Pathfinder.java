6bd7dbe824594b688f763bc7736636b818f7a009
Gabriele Giuntoli <lelegiunto1@gmail.com>
2020-09-11 01:02:54 +0200

    commit

--- src/generatore_pg/Generatore_Pg_Pathfinder.java
+++ src/generatore_pg/Generatore_Pg_Pathfinder.java
@@ -1,20 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package generatore_pg;
 
-/**
- *
- * @author leleg
- */
+
 import java.util.Random;
 public class Generatore_Pg_Pathfinder{
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Random random = new Random();
 
