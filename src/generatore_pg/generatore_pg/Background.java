c82574fc93123ae0953a82f21cb4f17156a69ecd
Gabriele Giuntoli <lelegiunto1@gmail.com>
2020-10-26 00:58:54 +0100

    commit

--- src/generatore_pg/generatore_pg/Background.java
+++
@@ -1,9 +0,0 @@
-package generatore_pg;
-import java.util.Random;
-
-public class Background {
-    Random random=new Random();
-    String[] Background = new String[] {"Pirulo","Pistrolo"};
-    int b=random.nextInt(Background.length);
-    
-}
