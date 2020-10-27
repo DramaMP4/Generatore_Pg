c196e7e03469f4852511e73e1b45c7ccd3409d01
Gabriele Giuntoli <lelegiunto1@gmail.com>
2020-10-27 15:15:49 +0100

    commit

--- src/generatore_pg/Incantesimi.java
+++ src/generatore_pg/Incantesimi.java
@@ -1,8 +1,14 @@
 import java.util.Random;
 
 
-public class Incantesimi {
-    Random random=new Random();
+public class Incantesimi{
+    public static void main(String[] args){
+        Random random=new Random();
+        String[] BardoTrucchetti=new String[]{""};
+        String[] BardoLVL1=new String[]{""};
+        String[] BardoLVL2=new String[]{""};
+        String[] BardoLVL3=new String[]{""};
 
-    String[] BardoTrucchetti=new String[]{""};
+    }
+    
 }
