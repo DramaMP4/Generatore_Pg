207812d7ed858c414a951ea248292b7567010ea2
Gabriele Giuntoli <65399190+DramaMP4@users.noreply.github.com>
2021-01-17 16:57:03 +0100

    commit

--- src/generatore_pg/generatore_pg/Generatore_Pg_DnD_5E.java
+++ src/generatore_pg/generatore_pg/Generatore_Pg_DnD_5E.java
@@ -159,7 +159,7 @@ public class Generatore_Pg_DnD_5E{
             
         }else
         {
-            System.out.println("Livello troppo basso o troppo alto \n Ricorda il livelli vanno da 1 a 20 ");
+            System.out.println("Livello troppo basso per avere una sottoclasse \n Ricorda il livello per la sottoclasse è 3 ");
         }
         
     }
