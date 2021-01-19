47546dbf9a04c805e74728acad113aababd26517
Gabriele Giuntoli <65399190+DramaMP4@users.noreply.github.com>
2021-01-19 12:16:20 +0100

    commit

--- src/generatore_pg/generatore_pg/Generatore_Magie.java
+++ src/generatore_pg/generatore_pg/Generatore_Magie.java
@@ -1,7 +1,10 @@
 package generatore_pg;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Generatore_Magie{
-
+    Scanner input = new Scanner(System.in);
+    Random random = new Random();
+    
 }
